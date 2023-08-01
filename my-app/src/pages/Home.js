af39a804434d7363d4fa3205ea8f93d96eb5c30f
Alex <akp42@duke.edu>
2023-07-31 23:34:12 -0400

    wip

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -3,7 +3,224 @@ export default function Home() {
     <>
       <div>
         <h1> here is home</h1>
+        <div id="intro">
+        <h1>Alex Pieroni</h1>
+        <h3>Trinty College of Arts and Sciences, Duke University 2025</h3>
+        <p>
+          B.A., Computer Science + Linguistics & B.A., in Classical Languages
+        </p>
+        <ul className="actions">
+          <li>
+            <a href="#header" className="button icon solid solo fa-arrow-down scrolly">
+              Continue
+            </a>
+          </li>
+        </ul>
+      </div>
+
+
+
       </div>
     </>
   );
 }
+
+
+// 
+  /* 
+      <header id="header">
+        <a href="index.html" className="logo">
+          GET CURIOUS.
+        </a>
+      </header>
+      <nav id="nav">
+        <ul className="links">
+          <li className="active">
+            <a href="index.html">Projects</a>
+          </li>
+          <li>
+            <a href="generic.html">ABOUT ME & RESUME</a>
+          </li>
+        </ul>
+        <ul className="icons">
+          <li>
+            <a href="https://www.linkedin.com/in/alex-pieroni/" className="icon brands alt fa-linkedin">
+              <span className="label">LinkedIn</span>
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/ajpieroni" className="icon brands alt fa-github">
+              <span className="label">GitHub</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div id="main">
+        <article className="post featured">
+          <header className="major">
+            <h2>
+              <a href="https://github.com/ajpieroni/chatmedea">
+                ChatMedea
+                <br />
+                <h3>HuggingFace Transformers Library</h3>
+              </a>
+            </h2>
+            <p>
+              An interactive chatbot powered by a fine-tuned Diablo GPT model, offering engaging conversations based on Euripides'
+              Medea, created during the Out in Tech Mentorship Program.
+            </p>
+          </header>
+          <a href="https://github.com/ajpieroni/chatmedea" className="image main">
+            <img src="images/medea.png" alt="ChatMedea demo" />
+          </a>
+          <ul className="actions special">
+            <li>
+              <a href="https://chatmedea.glitch.me" className="button large">
+                View Live Demo
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/ajpieroni/chatmedea" className="button large">
+                View Repository
+              </a>
+            </li>
+          </ul>
+        </article>
+        <section className="posts">
+          <article>
+            <header>
+              <h2>
+                <a href="https://github.com/ajpieroni/route">
+                  Route
+                  <br />
+                  <h3>Java</h3>
+                </a>
+              </h2>
+            </header>
+            <a href="https://github.com/ajpieroni/route" className="image main">
+              <img src="images/Route.png" alt="" />
+            </a>
+            <p>
+              A project using an adapted Dijkstra's algorithm that finds the shortest path between any two cities in the US.
+            </p>
+            <ul className="actions special">
+              <li>
+                <a href="https://github.com/ajpieroni/route" className="button large">
+                  View Project
+                </a>
+              </li>
+            </ul>
+          </article>
+          <article>
+            <header>
+              <h2>
+                <a href="https://github.com/ajpieroni/nychealth">
+                  Investigating NYC Health Inspections
+                  <br />
+                  <h3>R</h3>
+                </a>
+              </h2>
+            </header>
+            <a href="https://github.com/ajpieroni/nychealth" className="image fit">
+              <img src="images/NYC.png" alt="" />
+            </a>
+            <p>
+              What boroughs have the highest and lowest health ratings, and how does that correlate to median income? This data is
+              from the New York Open Data Portal, sourced from TidyTuesday.
+            </p>
+            <ul className="actions special">
+              <li>
+                <a href="https://github.com/ajpieroni/nychealth" className="button">
+                  View Project
+                </a>
+              </li>
+            </ul>
+          </article>
+          <article>
+            <header>
+              <h2>
+                <a href="https://github.com/ajpieroni/BreadAlgorithm">
+                  Bread Calculator
+                  <br />
+                  <h3>Java</h3>
+                </a>
+              </h2>
+            </header>
+            <a href="https://github.com/ajpieroni/BreadAlgorithm" className="image fit">
+              <img src="images/bread.jpg" alt="" />
+            </a>
+            <p>A Java program using Java Swing GUI that calculates a bread cooking timeline for my famous focaccia bread.</p>
+            <ul className="actions special">
+              <li>
+                <a href="https://github.com/ajpieroni/BreadAlgorithm" className="button">
+                  View Project
+                </a>
+              </li>
+            </ul>
+          </article>
+        </section>
+        <footer id="footer">
+          <section>
+            <form method="post" action="#">
+              <div className="fields">
+                <div className="field">
+                  <label htmlFor="name">Name</label>
+                  <input type="text" name="name" id="name" />
+                </div>
+                <div className="field">
+                  <label htmlFor="email">Email</label>
+                  <input type="text" name="email" id="email" />
+                </div>
+                <div className="field">
+                  <label htmlFor="message">Message</label>
+                  <textarea name="message" id="message" rows="3"></textarea>
+                </div>
+              </div>
+              <ul className="actions">
+                <li>
+                  <input type="submit" value="Send Message" />
+                </li>
+              </ul>
+            </form>
+          </section>
+          <section className="split contact">
+            <section className="alt">
+              <h3>Location</h3>
+              <p>Dallas, TX</p>
+            </section>
+            <section>
+              <h3>Phone</h3>
+              <p>
+                <a href="#">(214) 808-7517</a>
+              </p>
+            </section>
+            <section>
+              <h3>Email</h3>
+              <p>
+                <a href="#">alexander.pieroni@duke.edu</a>
+              </p>
+            </section>
+            <section>
+              <h3>Social</h3>
+              <ul className="icons alt">
+                <li>
+                  <a href="https://www.linkedin.com/in/alex-pieroni/" className="icon brands alt fa-linkedin">
+                    <span className="label">LinkedIn</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://github.com/ajpieroni" className="icon brands alt fa-github">
+                    <span className="label">GitHub</span>
+                  </a>
+                </li>
+              </ul>
+            </section>
+          </section>
+        </footer>
+        <div id="copyright">
+          <ul>
+            <li>&copy; Alex Pieroni</li>
+          </ul>
+        </div>
+      </div> */
+
